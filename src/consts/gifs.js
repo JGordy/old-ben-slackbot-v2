60b3d2d31fda8ddbb1c020022a01b1bc8f31e296
Joseph Gordy <jgordy2424@gmail.com>
2021-08-30 23:47:57 -0400

    Add lots of Obi Wan gifs

---
+++ src/consts/gifs.js
@@ -0,0 +1,26 @@
+const giphyUrlBase = 'https://media.giphy.com/media/';
+
+const obiWanGifs = {
+    absolutes: `${giphyUrlBase}uNgUzhakqXkyI/giphy.gif?cid=790b7611e4fc315682210b1507bad6fcb908e0fa3af110a9&rid=giphy.gif&ct=g`,
+    anotherHappyLanding: `${giphyUrlBase}Dmydf2Zf2kOys/giphy.gif?cid=790b761120d898f3e5b091fcf545fc6f685d6e429f15b351&rid=giphy.gif&ct=g`,
+    chosenOne: `${giphyUrlBase}14tkWw2VQSxfry/giphy.gif?cid=790b7611b7b3fc16101f2a982c01a504e17ca3caa2f60460&rid=giphy.gif&ct=g`,
+    dontTryIt: `${giphyUrlBase}xTiIzzYsS5UeSANFbW/giphy.gif?cid=790b76110f6e7847b394c9f6cb7b81b7bab27aa1506a22fc&rid=giphy.gif&ct=g`,
+    goodCall: `${giphyUrlBase}3ohuAx4bMIeoa2LcXu/giphy.gif?cid=790b7611bc7a584c41d502711ae13cff655f693d97203dec&rid=giphy.gif&ct=g`,
+    goodJob: `${giphyUrlBase}l1KsJua7qIQnevGyk/giphy.gif?cid=790b761138be9881dbe347e6aae5edf2f08a516f38dc0207&rid=giphy.gif&ct=g`,
+    onEdge: `${giphyUrlBase}l1KsQ6z4wHrt5ARuo/giphy.gif?cid=790b76116c27774c4a7b214c99fde98ea502134672fa6987&rid=giphy.gif&ct=g`,
+    helloThereOld: `${giphyUrlBase}3ornk57KwDXf81rjWM/giphy.gif?cid=ecf05e47184rp3s6zrxmehgic5cby3e9bi0mqg3mvc55wt19&rid=giphy.gif&ct=g`,
+    helloThereYoung: `${giphyUrlBase}xTiIzJSKB4l7xTouE8/giphy.gif?cid=ecf05e47r61dtjtotvu5v1ywiz8ugvjmm2roj0yfy2g5nf3y&rid=giphy.gif&ct=g`,
+    joyful: `${giphyUrlBase}9hEZaVdGkpZte/giphy.gif?cid=790b761141639d2a3be00ca385c89cc9945829ea314d9db3&rid=giphy.gif&ct=g`,
+    lightsaberFlourish: `${giphyUrlBase}A6VV6wLneB3KU/giphy.gif?cid=790b76114c538f25e9629e5aa1bba96f447926ae691fa76c&rid=giphy.gif&ct=g`,
+    mustacheYoung: `${giphyUrlBase}12Gyz2J1b9SjD2/giphy.gif?cid=790b76118f646eea5aa00c56d2217c4c1312c502d8f60d87&rid=giphy.gif&ct=g`,
+    mustacheOld: `${giphyUrlBase}hdvELDjb9rwNa/giphy.gif?cid=790b7611ec5d4b8bc4a921693abd572c985becb564714f0d&rid=giphy.gif&ct=g`,
+    nooo: `${giphyUrlBase}3owzWks5HVpaVtjjri/giphy.gif?cid=790b7611d4c9bfb25fb68f564ce9fab142048628415a5c3b&rid=giphy.gif&ct=g`,
+    nope: `${giphyUrlBase}GjcDoK2aAA0zC/giphy.gif?cid=790b761144e11a3b4916c0090383f7b7158896fe04d03f82&rid=giphy.gif&ct=g`,
+    patience: `${giphyUrlBase}3ohuPcatcAKIYJCKBO/giphy.gif?cid=790b7611abd1f3e651a6d6d360cbe5007fef1cff09c6a77b&rid=giphy.gif&ct=g`,
+    soUncivilized: `${giphyUrlBase}xTiIzkLOknx8ELm4Ok/giphy.gif?cid=790b761109c44aff0ef19855404ac0f066efd05707cd2305&rid=giphy.gif&ct=g`,
+    smh: `${giphyUrlBase}3o84sx3H5SlSZ2qZdS/giphy.gif?cid=790b7611e8e251d1ce4ccac94bd7570b0ceeff21b08df5cc&rid=giphy.gif&ct=g`,
+    think: `${giphyUrlBase}l1KsOGE23suZQeCfm/giphy.gif?cid=790b76117def942b2b395ad84e6add4f4a561f5eafaa56ef&rid=giphy.gif&ct=g`,
+    what: `${giphyUrlBase}l1KsAVIlhsWoZC0Zq/giphy.gif?cid=790b76112a83ddd65b416f3c1f91d5f650c6d3f4977a16dd&rid=giphy.gif&ct=g`
+};
+
+module.exports = obiWanGifs;
