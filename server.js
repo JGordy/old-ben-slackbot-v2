42b08934c6e353bf66e2ea30fd259de89f311395
Joseph Gordy <jgordy2424@gmail.com>
2021-09-01 01:00:17 -0400

    Add log for error on app startup

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
-const app = require('./src/index.js');
+const app = require('./src/bot.js');
 
 const port = process.env.PORT || 8001;
+
+app.error((error) => {
+    // Check the details of the error to handle cases where you should retry sending a message or stop the app
+    console.error(error);
+});
 
 (async () => {
     // Start your app
